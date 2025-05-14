3e7fdf7cd2d3e6e88c0ce9907d7cee4e85115f0e
Joaquin-Huaranga <joaquinhuarangaorihuela@gmail.com>
2025-05-14 17:38:52 -0500

    add carousel

--- gilda-iestp/src/pages/home/index.jsx
+++ gilda-iestp/src/pages/home/index.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '../../components/ui/carousel.js';
 
 export const Home = () => {
-  return <div>HOLAAA</div>;
+  return (
+    <Carousel>
+      <CarouselContent>
+        <CarouselItem>
+          <img
+            src="https://www.iestpgildaballivian.edu.pe/Public/img/formacion1.jpg"
+            alt=""
+          />
+        </CarouselItem>
+        <CarouselItem>
+          <img
+            src="https://www.iestpgildaballivian.edu.pe/Public/img/titulad.jpg"
+            alt=""
+          />
+        </CarouselItem>
+        <CarouselItem>
+          <img
+            src="https://www.iestpgildaballivian.edu.pe/Public/img/gilda_noche.jpg"
+            alt=""
+          />
+        </CarouselItem>
+        <CarouselItem>
+          <img
+            src="https://www.iestpgildaballivian.edu.pe/Public/img/web66.jpg"
+            alt=""
+          />
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
 };
