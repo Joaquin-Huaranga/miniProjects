11b097442a58ece0168bd8d728bae61f5272aaa8
Owner <joaquinhuarangaorihuela@gmail.com>
2025-07-17 17:48:38 -0500

    add dynamic slug

--- nexu-core/src/data-list/index.js
+++ nexu-core/src/data-list/index.js
@@ -1,2 +1,3 @@
 export { default as cardComponents } from './card-components.json'
 export { default as peripherals } from './peripherals.json'
+export { default as processorsData } from './processors-data.json'
